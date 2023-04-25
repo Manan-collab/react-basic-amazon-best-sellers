10b4e8b7c6d4a76a50ea71dc09578670f189e64d
manan-coditas <manan.mer@coditas.com>
2023-04-25 11:24:45 +0530

    complete project

---
+++ src/books.tsx
@@ -0,0 +1,25 @@
+import img1 from './images/book-1.jpg';
+import img2 from './images/book-2.jpg';
+import img3 from './images/book-3.jpg';
+
+export const books = [
+    // https://www.amazon.com/Best-Sellers-Books/zgbs/books/
+    {
+      title: "Atomic Habits",
+      author: "James Clear",
+      img: img1,
+      id: 1,
+    },
+    {
+      title: "The 48 Laws of Power",
+      author: "Robert Greene",
+      img: img2,
+      id: 2,
+    },
+    {
+      title: "Oh, the Places You'll Go!",
+      author: "Dr. Seuss",
+      img: img3,
+      id: 2,
+    },
+  ];
